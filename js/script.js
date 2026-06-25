594c9ecffbd594b310211bf21ffb1b88f1c0dad7
HappiJr <liane.smits@gmx.de>
2026-06-25 14:15:45 +0200

    Add jumping focus to code-inputs
    The javascript makes the focus jump to the next input when you fill the
    current one and jump back to the previous one when pressing Backspace.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+function moveToNext(currentInput) {
+    if (currentInput.value.length >= currentInput.maxLength) {
+        const nextInput = currentInput.nextElementSibling;
+        if (nextInput && nextInput.tagName.toLowerCase() === 'input') {
+            nextInput.focus();
+        }
+    }
+}
+
+function moveToPrev(currentInput, e) {
+    if (e.key === 'Backspace' && currentInput.value.length === 0) {
+        const prevInput = currentInput.previousElementSibling;
+        if (prevInput && prevInput.tagName.toLowerCase() === 'input') {
+            prevInput.focus();
+        }
+    }
+}
